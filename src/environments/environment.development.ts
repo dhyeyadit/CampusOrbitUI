333501dea61e7dc785fc50d9c071ec1f3cddf812
Dhyey Sutariya <dhyeysutariya07@gmail.com>
2025-10-02 10:07:47 +0530

    Added env files to commit

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:7142/api',
+  msal: {
+    clientId: '80831b22-7ddb-4f59-9230-56384d567f3d',
+    tenantId:'433cd4ce-c27e-49d8-a23e-d216767f790c',
+    authority: 'https://login.microsoftonline.com/433cd4ce-c27e-49d8-a23e-d216767f790c',
+    redirectUri: 'http://localhost:4200',
+    scopes:['api://80831b22-7ddb-4f59-9230-56384d567f3d/CampusOrbit.Access']
+  }
+};
